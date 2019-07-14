2570824690740982748f5807827ccb6ab74cf9eb
Dan Cuomo <dacuo@microsoft.com>
2019-07-13 23:42:46 -0700

    fixing broken test

--- tests/unit/unit.tests.ps1
+++ tests/unit/unit.tests.ps1
@@ -16,16 +16,17 @@ Describe "$($env:repoName)-Manifest" {
         }
 
         'Get-NetView', 'Validate-DCB' | ForEach-Object {
+            $module = Find-Module -Name $_ -ErrorAction SilentlyContinue
+
             It "Should contain the $_ Module" {
                 $_ -in ($TestModule).RequiredModules.Name | Should be $true
             }
 
-            Remove-Variable $module -ErrorAction SilentlyContinue
-            $module = Find-Module -Name $_ -ErrorAction SilentlyContinue
-
             It "The $_ module should be available in the PowerShell gallery" {
                 $module | Should not BeNullOrEmpty
             }
+
+            Remove-Variable -Name Module -ErrorAction SilentlyContinue
         }
     }
 }
